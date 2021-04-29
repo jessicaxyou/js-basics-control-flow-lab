e32d18dda5e72339743fd1c641d1dfbe43be64da
jessica you <jessicaxytan@gmail.com>
2021-04-28 22:09:21 -0400

    commit tree 1c70ffa05382e878ab1f25ada561bd8ea80e1a2d

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(measurement){
+  if (measurement <= 400) {
+    return "This one is on me!";
+  } else if (measurement > 2000 && measurement < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else
+    return "No can do.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(name){
+  if (name === "NYC") {
+    return "Ok, sounds good.";
+  } else
+    return "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === "generous") {
+    return "Thank you so much."
+  } else if (tip === "not as generous") {
+    return "Thank you."
+  } else
+  return "Bye."
 }
